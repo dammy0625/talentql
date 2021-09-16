8e02f00a720625abdf328f86321e3e80c66c8c14
dammy0625 <muyiwalateef@gmail.com>
2021-09-16 22:49:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const ham=document.querySelector(".ham")
+const slider=document.querySelector(".slider")
+
+ham.onclick = ()=>{
+    slider.classList.toggle("slide")
+}
+slider.onclick = ()=>{
+    slider.classList.toggle("slide")
+}
