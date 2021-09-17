97993591cd7d4ed34bb67e06e12525f7ad48d457
dammy0625 <muyiwalateef@gmail.com>
2021-09-17 18:51:06 +0100

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const ham=document.querySelector(".ham")
 const slider=document.querySelector(".slider")
+const bold=document.querySelector(".slider .bold")
+const all = document.querySelectorAll(".cur li")
+
+
+
+
+all.forEach((item)=>{
+    item.onmouseover = (e)=>{
+        bold.innerText = e.target.innerText
+        
+    }
+})
 
 ham.onclick = ()=>{
     slider.classList.toggle("slide")
 }
-slider.onclick = ()=>{
-    slider.classList.toggle("slide")
-}
+
